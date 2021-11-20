2cf5d17bbe7da480ebcdc441c82bebc1665d852f
floating <gxhurry@outlook.com>
2021-11-20 21:42:16 +0800

    Updated

--- deploymentTool/application/generate.js
+++ deploymentTool/application/generate.js
@@ -4,6 +4,31 @@ const fs = require("fs");
 const path = require("path");
 const crypto = require("crypto");
 
+const getConfig = function() {
+    return {
+        "siteName": "YJZX",
+        "siteURL": "https://example.com",
+        "year": 2021,
+        "owner": "Float",
+        "licenseLink": "https://creativecommons.org/licenses/by-sa/4.0/",
+        "lisenseName": "CC-BY-SA 4.0",
+        "navs": [ {
+                "name": "HOME",
+                "link": "/"
+            }, {
+                "name": "WORK",
+                "link": "/work"
+            }, {
+                "name": "BLOG",
+                "link": "https://yjzxlclub.github.io/"
+            }, {
+                "name": "ABOUT",
+                "link": "/about"
+            }
+        ]
+    };
+}
+
 const generator = {
 
     getHash: function(filePath) {
@@ -70,18 +95,27 @@ const generator = {
 
         let allFile = [];
         try {
-            const rootDirs = fs.readDirSync("./source");
+            const rootDirs = fs.readDirSync("source");
             for (let i = 0; i < rootDirs.length; i++) {
                 const dirName = rootDirs[i];
-                const dirPath = path.join("./source", dirName);
-                const stat = fs.statSync(dirPath);
-                if (stat.isDirectory()) {
-                    const tmp = getSingleDirectoty(dirPath);
-                    allFile.append({
-                        name: dirName,
-                        files: tmp
-                    });
-                } 
+                const dirPath = path.join("source", dirName);
+                try {
+                    fs.accessSync(path.join(dirPath, "infos.json"));
+                    const stat = fs.statSync(dirPath);
+                    if (stat.isDirectory()) {
+                        const tmp = getSingleDirectoty(dirPath);
+                        allFile.append({
+                            name: dirName,
+                            files: tmp
+                        });
+                    } 
+                } catch (err) {
+                    if (err.code === 'ENOENT') {
+                        ;
+                    } else {
+                        throw err;
+                    }
+                }
             }
         } catch (err) {
             if (err.code === 'ENOENT') {
@@ -95,29 +129,25 @@ const generator = {
 
     getChanged: function() {
         /*
-            promise, resolve an object that included all changed or newly build page and site's object.
-            resolve: object: [
+            promise (async), resolve an object that included all changed or newly build page and site's object.
+            resolve (return): object: [
                 {
-                    name: '',
+                    name: '', // directory's name
                     files[
-                        '',
+                        '', // path relative to porject root directory
                         ...
                     ]
                 },
                 ...
             ]
-
-            I dont know how to describe in English
-            现在我需要进行这样一个操作：
-                获取所有文件的 hash 值，对于这些 hash 进行比对。最后还要等到所有 hash 算完之后返回。
         */
 
-        return new Promise(async function(resolve, reject) {
+        return async function() {
             const allFile = this.getAllFile();
             try {
-                const fileRec = JSON.parse(fs.readFileSync('../source/fileRecord.json'));
+                const fileRec = JSON.parse(fs.readFileSync('source/fileRecord.json'));
                 let rts = []; // returns, words faild to me QwQ
-                // pardon my for loop, I thought it is more understandable
+                // pardon my for loop, I thought they are more understandable to me than callbacks
                 for (let i = 0; i < allFile.length; i++) {
                     const dir = allFile[i];
                     let chandedFile = {
@@ -140,16 +170,47 @@ const generator = {
                         rts.push(chandedFile);
                     }
                 }
-                resolve(rts);
+                return rts;
             } catch (err) {
                 if(err.code === 'ENOENT' ) {
                     // fileRecord,json not exsists, should return all page or site
-                    resolve(allFile);
+                    return allFile;
                 } else {
-                    reject(err);
+                    throw err;
                 }
             }
-        });
+        }
+    },
+
+    getAllInfos: function() {
+        let allInfos = [];
+        try {
+            const rootDirs = fs.readDirSync("source");
+            for (let i = 0; i < rootDirs.length; i++) {
+                const dirName = rootDirs[i];
+                const dirPath = path.join("source", dirName);
+                try {
+                    let data = JSON.parse(fs.readFileSync(path.join(dirPath, "infos.json")));
+                    data.link = "/" + dirName;
+                    allInfos.append(data);
+                } catch (err) {
+                    if(err.code === 'ENOENT' ) {
+                        // fileRecord,json not exsists
+                        ;
+                    } else {
+                        throw err;
+                    }
+                }
+            }
+            return allInfos;
+        } catch (err) {
+            if(err.code === 'ENOENT' ) {
+                // fileRecord,json not exsists
+                return allInfos;
+            } else {
+                throw err;
+            }
+        }
     },
 
     recordFile: function(filePaths) {
@@ -158,41 +219,219 @@ const generator = {
 
             get file's hash then write into fileRecord.json
         */
-        return new Promise(async function(resolve, reject) {
+        return async function() {
             let rec = {};
             for (let i = 0; i < filePaths.length; i++) {
                 const filePath = filePaths[i];
                 const checksum = await this.getHash(filePath);
                 rec[filePath] = checksum;
             }
-            fs.writeFile("./source/fileRecord.json", JSON.stringify(rec) , err => {
+            fs.writeFile("source/fileRecord.json", JSON.stringify(rec) , err => {
                 if (err) {
-                    reject(err)
+                    throw err;
                 } else {
-                    resolve();
+                    return;
                 }
             })
-        })
+        }
     },
 
-    randerPage: function(pagePath){
+    renderPage: function(pagePath, pageTitle){
         /*
-            use ejs to summon file
+            use ejs to summon file, then copy
         */
+        const data = fs.readFileSync(pagePath);
+        const ejsTemptele = fs.readFileSync("layout/ejs/page/page.ejs");
+
+        const config = getConfig();
+
+        const html = ejs.render(ejsTemptele, {
+            page: {
+                content: data,
+                title: pageTitle
+            },
+            config: config
+        });
+
+        const pathPublic = path.join('public', path.relative('source', pagePath));
+        fs.writeFileSync(pathPublic, html);
+        console.log("Writen " + pathPublic);
     },
 
-    randerCommonFile: function(filePath){
-
+    renderCommonFile: function(filePath){
+        /*
+            Just copy them into ./public
+        */
+        
+        const pathPublic = path.join('public', path.relative('source', filePath));
+        fs.copyFileSync(filePath, pathPublic);
+        console.log("Writen " + pathPublic);
     },
 
-    randerAll: function() {
+    renderCategories: function(allItems) {
+        /*
+            arg: [{
+                    link: '/',
+                    title: '',
+                    description: '',
+                    category: '',
+                    auther: '',
+                    date: ''
+                },
+                ...
+            ],
+        */
+
+        let categories = {
+            ALL_ITEMS: []
+        };
+
+        for (let i = 0; i < allItems.length; i++) {
+            const item = allItems[i];
+            if (categories[item.category] === undefined) {
+                categories[item.category] = [];
+            }
+            categories[item.category].append(item);
+            categories.ALL_ITEMS.append(item);
+        }
+        
+        // render all category
+        const ejsTemptele = fs.readFileSync("layout/ejs/categories/categories.ejs");
+        const htmlOfAll = ejs.render(ejsTemptele, {
+            config: getConfig(),
+            page: {
+                title: "Categories"
+            },
+            category: {
+                categories: ["All"].concat(categoriesKeys),
+                active: "All",
+                items: categories.ALL_ITEMS
+            }
+        });
+        fs.writeFileSync("public/categories/index.html", htmlOfAll);
+
+        // render every single category
+        const categoriesKeys = Object.getOwnPropertyNames(categories);
+        for (let i = 0; i < categoriesKeys.length; i++) {
+            const key = categoriesKeys[i];
+            const items = categories[key];
+            const html = ejs.render(ejsTemptele, {
+                config: getConfig(),
+                page: {
+                    title: "Categories " + key
+                },
+                category: {
+                    categories: ["All"].concat(categoriesKeys),
+                    active: key,
+                    items: items
+                }
+            });
+            fs.writeFileSync(path.join("public/categories", key, "index.html"), html);
+        }
+    },
+
+    renderTagPage: function(tagName, items) {
+        /*
+            arg: tagname: <string>
+                 items: <as same as in renderCategories()>
+        */
+        const ejsTemptele = fs.readFileSync("layout/ejs/tags/tags.ejs");
+        const html = ejs.render(ejsTemptele, {
+            config: getConfig(),
+            page: {
+                title: 'Tag' + tagName
+            },
+            items: items
+        });
+        fs.writeFileSync(path.join("public/tags", tagName, "index.html"), html);
+    },
+
+    renderAllTagPage: function(allInfos) {
+        let infosGuideByTag = {};
+        for (let i = 0; i < allInfos.length; i++) {
+            const info = allInfos[i];
+            for (let j = 0; j < info.tags.length; j++) {
+                const tag = info.tags[j];
+                if (infosGuideByTag[tag] === undefined) {
+                    infosGuideByTag[tag] = [];
+                }
+                infosGuideByTag[tag].append(info);
+            }
+        }
+        const keys = Object.getOwnPropertyNames(infosGuideByTag);
+        for (let i = 0; i < keys.length; i++) {
+            const key = keys[i];
+            for (let j = 0; i < infosGuideByTag[key].length; j++) {
+                const info = infosGuideByTag[key][j];
+                this.renderTagPage(key, info);
+            }
+        }
+    },
+
+    renderTagGuiding: function(allInfos) {
+        /*
+            arg: <as same as in renderCategories()>
+        */
+        let allTags = [];
+        for (let i = 0; i < allInfos.length; i++) {
+            const info = allInfos[i];
+            for (let j = 0; j < info.tags.length; j++) {
+                const tag = info.tags[j];
+                allTags.append(tag);
+            }
+        }
+        const ejsTemptele = fs.readFileSync("layout/ejs/tags/tagGuiding.ejs");
+        const html = ejs.render(ejsTemptele, {
+            config: getConfig(),
+            page: {
+                title: "Tags"
+            },
+            tagGuiding: {
+                items: allTags
+            }
+        });
+        fs.writeFileSync("public/tags/index.html", html);
+    },
+
+    renderAll: function() {
         /*
             Read all changed directory in ../source,
-            then rander them according to their infos.json,
+            then render them according to their infos.json,
             finally put them in ../public directory,
         */
-
         
+        // read all file
+        return async function() {
+            const allChangedFileList = await this.getChanged();
+            // if a page or site's info have been changed, we need to update categroies and tags
+            let infoChanged = false;
+            for (let i = 0; i < allChangedFileList.length; i++) {
+                const changed = allChangedFileList[i];
+                const infos = JSON.parse(fs.readFileSync(path.join("source", changed.name)));
+
+                for (let j = 0; j < changed.files.length; j++) {
+                    const filePath = changed.files[j];
+                    if ("infos.json" === path.relative(path.join("source", changed.name), filePath)) {
+                        // is infos
+                        infoChanged = true;
+                    } else if ( infos.type === "page"  &&
+                                "index.html" === path.relative(path.join("source", changed.name), filePath)) {
+                        // is page's index.html
+                        this.renderPage(filePath, infos.title);
+                    } else {
+                        this.renderCommonFile(filePath);
+                    }
+                }
+            }
+
+            // if infos have been changed, all categroies and tags should be updated
+            if (infoChanged) {
+                const allInfos = this.getAllInfos();
+                this.renderCategories(allInfos);
+                this.renderTagGuiding(allInfos);
+                this.renderAllTagPage(allInfos);
+            }
+        }
     }
 }
 
